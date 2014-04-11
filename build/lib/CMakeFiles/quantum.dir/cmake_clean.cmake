e7ef3bdad3aefd620dcadb014eb389f858a08699
kcoltin <kevincoltin@gmail.com>
2014-04-10 22:34:44 -0400

    added build directory

---
+++ build/lib/CMakeFiles/quantum.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/quantum.dir/__/src/algorithms.cpp.o"
+  "CMakeFiles/quantum.dir/__/src/gate_factory.cpp.o"
+  "CMakeFiles/quantum.dir/__/src/quantum.cpp.o"
+  "CMakeFiles/quantum.dir/__/src/util.cpp.o"
+  "../../lib/libquantum.pdb"
+  "../../lib/libquantum.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/quantum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/test/CMakeFiles/run_tests.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/run_tests.dir/run_tests.cpp.o"
+  "CMakeFiles/run_tests.dir/test_gate_factory.cpp.o"
+  "CMakeFiles/run_tests.dir/test_qubitsystem.cpp.o"
+  "CMakeFiles/run_tests.dir/test_quantumgate.cpp.o"
+  "CMakeFiles/run_tests.dir/test_algorithms.cpp.o"
+  "../../test/run_tests.pdb"
+  "../../test/run_tests"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/run_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
